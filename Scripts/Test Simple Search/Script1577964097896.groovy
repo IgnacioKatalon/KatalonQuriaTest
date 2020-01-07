2177aafe30859f3db4932d241c0e75847a20c84a
IgnacioTejera <45078940+IgnacioTejera@users.noreply.github.com>
2020-01-07 13:19:19 +0100

    Search test cases new

--- Scripts/Test Simple Search/Script1577964097896.groovy
+++ Scripts/Test Simple Search/Script1577964097896.groovy
@@ -27,7 +27,7 @@ WebUI.click(findTestObject('Object Repository/Test Simple Search/Page_Logg p  Go
 WebUI.setEncryptedText(findTestObject('Object Repository/Test Simple Search/Page_Logg p  Google-kontoer/input_For mange mislykkede for'), 
     'd097ITWPO7f2v9EptRJGCg==')
 
-WebUI.clickImage(findTestObject('Test Simple Search/Page_Logg p  Google-kontoer/span_Neste'))
+WebUI.click(findTestObject('Test Simple Search/Page_Logg p  Google-kontoer/div_Neste'))
 
 WebUI.click(findTestObject('Object Repository/Test Simple Search/Page_nightlyno - Axiell Quria/md-icon'))
 
---
+++ Test Suites/Test Suite Quria Search.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Quria Search</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>22213525-9f46-4ac8-bf61-2811f11c00e7</testSuiteGuid>
+   <testCaseLink>
+      <guid>97521ddc-0043-4010-9752-8bb158a59174</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Simple Search</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2fc462a5-40ce-4d8c-a990-475c3deab23a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Facet</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d48c354a-648e-4bf9-ab7b-1d63c31fe11d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Single Search hit, only one hit result</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8b2e18d6-cc24-4044-ab33-4f465be6d06f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test turn off stop word</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>032dbe52-c41c-45e5-a637-d340ac60c7f2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test verify free text search</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b9e65f88-b48e-4189-bebb-deb5995025e6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test verify Fuzzy search</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e39dfe03-4a62-44fd-af3b-799ba5783414</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test verify phrase search</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>872d0ae1-96f6-4d5c-b149-0e3ced8890dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test verify scandinavian letters</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d66ff84e-2ee3-41cf-b30b-f98c227f870c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test verify stop words</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d1fc8cdd-580f-4994-ac35-7c33a8196f53</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test verify truncation and masking</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
